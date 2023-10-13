195d3be2ec5f44c74b5ed91a6f5eae16adce490e
Yeonseung Yoo <85042907+Yonn-Yoo@users.noreply.github.com>
2023-10-13 09:33:02 +0900

    feat: declare server

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,7 +1,20 @@
+import { db } from '@/lib/db';
 import { initialProfile } from '@/lib/initial-profile';
+import { redirect } from 'next/navigation';
 
 export default async function SetupPage() {
   const profile = await initialProfile();
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  server && redirect(`/servers/${server.id}`);
 
   return (
     <div>
