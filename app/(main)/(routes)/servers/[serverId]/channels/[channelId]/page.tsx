744022b12693eccc68c517ce98ea6aa1d36e4156
Yeonseung Yoo <85042907+Yonn-Yoo@users.noreply.github.com>
2023-10-16 15:37:16 +0900

    feat: create chat input component

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,4 +1,5 @@
 import ChatHeader from '@/components/chat/chat-header';
+import ChatInput from '@/components/chat/chat-input';
 import { currentProfile } from '@/lib/current-profile';
 import { db } from '@/lib/db';
 import { redirectToSignIn } from '@clerk/nextjs';
@@ -39,6 +40,16 @@ export default async function ChannelIdPage({ params }: Props) {
         serverId={channel.serverId}
         type="channel"
         name={channel.name}
+      />
+      <div className="flex-1">future messages</div>
+      <ChatInput
+        apiUrl="/api/socket/messages"
+        query={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
+        name={channel.name}
+        type="channel"
       />
     </div>
   );
